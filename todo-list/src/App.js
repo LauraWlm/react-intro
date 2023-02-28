57b180995982a123bb41700d5a71d83632e92354
Laura Wilhelmi <laurawilhelmi.pro@gmail.com>
2023-02-28 16:47:27 +0100

    test localstorage

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import './style/App.scss';
 import './style/Form.scss';
 import './style/TodoList.scss';
@@ -6,14 +6,24 @@ import './Header/Header';
 import Form from './Form/Form';
 import TodoList from './TodoList/TodoList';
 import Header from "./Header/Header";
+import { v4 as uuidv4 } from "uuid";
 
+const LSKEY = "MyTodoApp";
 
 function App() {
+  // Initialize the state
   const [todos, setTodos] = useState([]);
 
+  // Update the state
   const addTodo = (text) => {
-    setTodos((prevTodos) => [...prevTodos, { text, checked: false }]);
-  };
+    setTodos((prevTodos) => [...prevTodos, { id: uuidv4(), text, checked: false }]);
+  }
+
+  //Save todos to localStorage
+  useEffect(() => {
+    window.localStorage.setItem(LSKEY + "todos", JSON.stringify(todos))
+  }, [todos]);
+
 
   return (
     <div className="app">
